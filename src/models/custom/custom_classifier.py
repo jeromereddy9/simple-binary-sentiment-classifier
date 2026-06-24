bdd83845ef42d3f1dc6527915a54a2e91fada31f
JPR <72471169+jeromereddy9@users.noreply.github.com>
2026-06-24 11:28:57 +0200

    Update custom_classifier.py

---
+++ src/models/custom/custom_classifier.py
@@ -0,0 +1,48 @@
+import os, sys
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+from src.preprocessing import Preprocessor
+import pickle as pkl
+import numpy as np
+import random as r
+from src.utils import path_builder
+
+
+class Custom_Classifier:
+    def __init__(self,preprocessor,dim=8):
+        self.preprocessor = preprocessor
+        self.dim = dim
+        self.vocab = preprocessor.get_vocab()
+        self.embedded_vocab = self.build_embedded_vocab()
+
+    def build_embedded_vocab(self):
+        rows, cols = self.vocab.shape()
+        embedded_vocab = np.zeros((rows,cols),dtype=1)
+        for i in range(rows):
+            for j in range(cols):
+                if self.vocab[i][j] == '':
+                    embedded_vocab[i][j] = [0] * self.dim
+                else:
+                    embedded_vocab[i][j] = [r.uniform(-0.1, 0.1) for i in range(self.dim)]
+
+        return embedded_vocab
+
+    def load_embedded_vocab(self,path):
+        try:
+            self.embedded_vocab = pkl.load(open(path_builder(path), "rb"))
+        finally:
+            self.embedded_vocab = pkl.load(open(path, "rb"))
+
+
+    def save_embedded_vocab(self,path):
+        try:
+            pkl.dump(self.embedded_vocab, open(path_builder(path), "wb"))
+        finally:
+            pkl.dump(self.embedded_vocab, open(path, "wb"))
+
+    
+
+
+        
+        
