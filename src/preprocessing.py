9524e0aa97d624777162d11694df82fcf446eda0
JPR <72471169+jeromereddy9@users.noreply.github.com>
2026-06-25 21:12:02 +0200

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,14 +6,19 @@ from utils import path_builder
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.layers import TextVectorization
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 def clean_data(data):
     data_lower = tf.strings.lower(data)
     return tf.strings.regex_replace(data_lower,r'[^a-zA-Z0-9\s?!]','')
 
 class Preprocessor:
-    def __init__(self,dataset_path,max_sequence_length=12):
+    def __init__(self,dataset_path=None,max_sequence_length=12,training_split=0.7,val_split=0.15,test_split=0.15):
         self.dataset_path = dataset_path
+        self.training_size = training_split
+        self.test_size = test_split
+        self.val_size = val_split
         self.data = pd.read_csv(path_builder(dataset_path),encoding='utf-8',dtype='str')
         self.max_sequence_length = max_sequence_length
         self.vectorizer = TextVectorization(
@@ -23,6 +28,7 @@ class Preprocessor:
             output_sequence_length=self.max_sequence_length
         )
         self.raw_text = self.data.iloc[:,0].values
+        self.labels = self.data.iloc[:,1].values
         self.vectorizer.adapt(self.raw_text)
 
     def get_cleaned_data(self):
@@ -31,12 +37,28 @@ class Preprocessor:
     def get_vocabulary(self):
         return self.vectorizer.get_vocabulary()
 
+    def preprocess_sequence(self,sequence):
+        return (self.vectorizer(sequence)).numpy()
+
+    def get_train_test_val_splits(self):
+        np.random.seed(42)
+        x = self.get_cleaned_data()
+        y = self.labels
+        x_temp,x_test,y_temp,y_test = train_test_split(x,y,test_size=self.test_size,random_state=42,stratify=y)
+        x_train,x_val,y_train,y_val = train_test_split(x,y,test_size=self.val_size,random_state=42,stratify=y_temp)
+
+        return x_test,x_train,x_val,y_test,y_train,y_val
+
+
 
 
 
 path = 'data/IMDB Dataset.csv'
 p = Preprocessor(path)
 
+print(p.get_cleaned_data()[0])
+print(p.get_cleaned_data()[5])
+print((p.get_vocabulary()))
 
 
 
